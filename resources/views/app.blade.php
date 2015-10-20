8e9e8c3c41909fa0b4cb0baa5ff3f982cae69858
Guo Yunhe <guoyunhebrave@gmail.com>
2015-10-20 23:33:48 +0300

    App page header.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,18 +5,28 @@
 @section('main')
 <div id="app-main">
     <div class="container">
-        <header>
-            <img class="icon" src="{{ $icon }}">
-            <div class="text">
+        <header class="app-header">
+            <div class="app-icon"><img class="icon" src="{{ $icon }}"></div>
+            <div class="app-info">
                 <h1 class="name">{{ $name }} <small>{{ $version }}</small></h1>
-                <div class="developer-info">By {{ $developer }}, since {{ $start }}. Use <a href="{{ $license_url }}">{{ $license }}</a>.</div>
+                <div class="developer-info">{{ $developer }}, since {{ $start }}</div>
+                <div class="license"><a href="{{ $license_url }}">{{ $license }}</a></div>
                 <div class="review-info">
                     @include('parts.rate', ['rate' => $rate])
                     <span class="review-count">{{ count($reviews) }} reviews</span>
                 </div>
-                <button class="btn btn-primary btn-raised" title="Install through your package manager">Install Package</button>
-                <a class="btn btn-default btn-raised" title="Download general binary package">Download</a>
-                <a class="btn btn-default btn-raised" title="Download source code">Source Code</a>
+                <div class="tags">
+                    @foreach($tags as $tag_code => $tag_name)
+                        <a class="label label-info" href="/tag/{{ $tag_code }}">{{ $tag_name }}</a>
+                    @endforeach
+                </div>
+                <div class="brief-desc"><big>{{ $brief_desc }}</big></div>
+                <div class="actions">
+                    <a class="btn btn-primary" title="Install through your package manager"
+                       href="{{ $install_uri }}">Install</a>
+                    <a class="btn btn-default" title="Download general binary package">Download</a>
+                    <a class="btn btn-default" title="Download source code">Source Code</a>
+                </div>
             </div>
         </header>
     </div>
