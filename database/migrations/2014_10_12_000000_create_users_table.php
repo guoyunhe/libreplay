09af7aab9e42a8b8df99bab86876b233616f6b59
Guo Yunhe <guoyunhebrave@gmail.com>
2015-10-28 23:11:56 +0200

    Restrict length of email and name, since unique key of utf8mb4 is larger than utf8.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('name', 100)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
